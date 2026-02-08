47fab50b9cb787a0f2c4e31f42e5768a849e8f38
mark-well <lacemerto05@gmail.com>
2026-02-08 12:20:21 +0800

    deploy:miss

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
                 <main className="message px-8 mt-8 text-justify flex flex-col items-center gap-4 indent-8">
                     <p>Oyy? ano at napadpad ka dito? miss mo ako no?🤭 ikaw ha.</p>
                     <p>Hindi ko alam kung babalikan mo pa tong website na ‘to, pero kung nababasa mo ‘to ibig sabihin non binalikan mo nga HAHAHAHA.</p>
-                    <p>Feel free to message me if kailangan mo ng kausap. Kung ayaw mo edi... bahala ka HAHAHA</p>
+                    {/* <p>Feel free to message me if kailangan mo ng kausap. Kung ayaw mo edi... bahala ka HAHAHA</p> */}
                 </main>
                 <MusicLayer className="mt-8" />
             </div>
